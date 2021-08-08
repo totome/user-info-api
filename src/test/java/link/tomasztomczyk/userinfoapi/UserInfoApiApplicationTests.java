d41d354dd1f095847ad325158a621b37c1b6f95b
Tomasz Tomczyk <ttomczyk87@gmail.com>
2021-08-08 19:30:05 +0200

    Default project with dependencies for JAX-RS and JPA initialized

---
+++ src/test/java/link/tomasztomczyk/userinfoapi/UserInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package link.tomasztomczyk.userinfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInfoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
